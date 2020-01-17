4f4aae11a0fe9e188760b331db6150090b817d9b
sooshe <sooshe@sooshebot.com>
2020-01-17 14:07:26 -0600

    import app functions to test

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render } from "@testing-library/react";
+// import ReactDOM from 'react-dom';
 import App from './App';
+import { addHits, addRuns, addPitchWin, addFoulBall } from './App';
 
 test('renders without crashing', () => {
   render(<App />);
+});
+
+test('Title card is visible', () => {
+  const { findAllByText } = render(<App />);
+    findAllByText(/title-card/i);
 });
