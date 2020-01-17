86272c4033a9ec5e648d2058504536217174951d
sooshe <sooshe@sooshebot.com>
2020-01-17 14:13:32 -0600

    addPitchWin test works

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -16,12 +16,19 @@ test('adds 1 point to the Lions current score on click', () => {
   let actual = addHits(0);
   let expected = 1;
   expect(actual).toBe(expected);
-  expect(actual).not.toBe(5);
+  expect(actual).not.toBe(50);
 });
 
-test('Adds 4 point to the Lions current score on click', () => {
+test('adds 4 point to the Lions current score on click', () => {
   let actual = addRuns(0);
   let expected = 4;
   expect(actual).toBe(expected);
   expect(actual).not.toBe(9);
+});
+
+test('adds 3 points to Lions current score on click', () => {
+  let actual = addPitchWin(0);
+  let expected = 3;
+  expect(actual).toBe(expected);
+  expect(actual).not.toBe(4);
 });
