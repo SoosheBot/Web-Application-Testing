e50047ee54ca5d0dbc77aaedddb9f9184429f52c
sooshe <sooshe@sooshebot.com>
2020-01-17 14:10:53 -0600

    third test working

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from "@testing-library/react";
-// import ReactDOM from 'react-dom';
 import App from './App';
 import { addHits, addRuns, addPitchWin, addFoulBall } from './App';
 
@@ -11,4 +10,12 @@ test('renders without crashing', () => {
 test('Title card is visible', () => {
   const { findAllByText } = render(<App />);
     findAllByText(/title-card/i);
+});
+
+test('adds 1 point to the Lions current score on click', () => {
+  let actual = addHits(0);
+  let expected = 1;
+
+  expect(actual).toBe(expected);
+  expect(actual).not.toBe(5);
 });
